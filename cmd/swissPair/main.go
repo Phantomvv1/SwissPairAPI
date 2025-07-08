d4df38b128fbc12296a4c05f9627ca7b67a06985
Phantomvv1 <krisi_andreev@abv.bg>
2025-07-08 13:21:54 +0300

    Adding the player endpoints to the router

--- cmd/swissPair/main.go
+++ cmd/swissPair/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	. "github.comPhantomvv1/SwissPairAPI/internal/auth"
+	. "github.comPhantomvv1/SwissPairAPI/internal/players"
 	. "github.comPhantomvv1/SwissPairAPI/internal/tournament"
 )
 
@@ -17,13 +18,18 @@ func main() {
 	r.POST("/profile", GetCurrentProfile)
 	r.DELETE("/account", DeleteAccount)
 
-	t := r.Group("tournament")
+	t := r.Group("/tournament")
 	t.GET("/", GetAllTournaments)
 	t.POST("/", CreateTournament)
 	t.POST("/get", GetTournament)
 	t.PUT("/", UpdateTournament)
 	t.DELETE("/", DeleteTournament)
 	t.POST("/status", GetTournamentsWithStatus)
+
+	p := r.Group("/player")
+	p.POST("/", CreatePlayer)
+	p.POST("/get", GetPlayersForTournament)
+	p.DELETE("/", RemoveUserFromTournament)
 
 	r.Run(":42069")
 }
